501e7d7a7b8d783d4c414bc7cb82667c85544354
d1lmurodvic <dildoraishmuhamedova4@gmail.com>
2025-08-21 02:57:42 +0500

    NEXT SARDOR PROJECT

---
+++ src/app/page.js
@@ -0,0 +1,9 @@
+
+
+export default function Home() {
+  return (
+    <div className="flex items-center justify-center h-screen text-6xl">
+      <h1>Sardor is <span className="text-red-500">Best</span> of the <span className="text-red-500">Best</span></h1>
+    </div>
+  );
+}
