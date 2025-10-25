e03c1de331ded82c5f181273e78e760f11c9a0ec
d1lmurodvic <dildoraishmuhamedova4@gmail.com>
2025-10-25 19:59:45 +0500

    asdf

--- src/app/products/page.jsx
+++ src/app/products/page.jsx
@@ -2,14 +2,15 @@ import Container from "@/components/container/Container";
 
 
 export default function Products() {
+
   return (
     <Container>
       <div className="my-6 flex gap-5">
 
-        <div className="border-2 border-info animate-pulse  flex flex-wrap w-[100%] gap-4  p-2 rounded-2xl ">
+        <div className="border-2 border-info animate-pulse grid grid-cols-5 w-[100%] gap-4  p-2 rounded-2xl ">
           <div>
             <div className="gap-2">
-              <div className="card card-xs h-[38vh] bg-base-200 max-w-60  p-2 border border-info shadow">
+              <div className="card card-xs h-[38vh] bg-base-200 max-w-60 flex  p-2 border border-info shadow">
                 <figure className="hover-gallery border-1 rounded-xl">
                   <img src="https://m.media-amazon.com/images/I/61JZV6C0DYL._UY1000_.jpg" className="w-10 h-10" />
                   <img src="https://m.media-amazon.com/images/I/61JZV6C0DYL._UY1000_.jpg" />
@@ -64,6 +65,40 @@ export default function Products() {
                 </div>
               </div>
             </div>
+
+            
+          </div>
+
+          <div>
+            
+            <div className="gap-2">
+              <div className="card card-xs h-[38vh] bg-base-200 max-w-60  p-2 border border-info shadow">
+                <figure className="hover-gallery border-1 rounded-xl">
+                  <img src="https://m.media-amazon.com/images/I/61JZV6C0DYL._UY1000_.jpg" className="w-10 h-10" />
+                  <img src="https://m.media-amazon.com/images/I/61JZV6C0DYL._UY1000_.jpg" />
+                  <img src="https://m.media-amazon.com/images/I/61JZV6C0DYL._UY1000_.jpg" />
+                  <img src="https://m.media-amazon.com/images/I/61JZV6C0DYL._UY1000_.jpg" />
+                </figure>
+                <div className="card-body border-1 rounded-b-xl ">
+                  <div className="flex flex-col">
+                    <p className="card-title flex justify-between text-warning">
+                      Sunglasses
+                      <span className=" text-warning  font-bold underline underline-offset-4">$25</span>
+                    </p>
+                  </div>
+                 <div className="flex">
+                  <div>
+                  <p >High Quality classic cap hat with stitch logo</p>           
+                  </div>
+                  <div>
+                    <button className="btn  btn-xs rounded-xl btn-success">Buy</button>
+                  </div>
+                </div>
+                </div>
+              </div>
+            </div>
+
+            
           </div>
           <div>
             <div className="gap-2">
--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -6,66 +6,85 @@ import ThemeController from "../ui/theme/themeController/page";
 import NavDesktop from "./NavDesktop";
 import NavMobile from "./NavMobile";
 import Link from "next/link";
+import { useRouter } from "next/navigation"; 
 
 export default function Header() {
-    return (
-        <header className="w-full shadow-md sticky top-0 z-50 bg-base-300 border-b-2 border-info rounded-b-xl">
-            <Container>
-                <div className="navbar flex justify-between items-center">
-                    <div className="flex items-center gap-2 cursor-pointer">
-                        <img
-                            src="/alifshop.svg"
-                            alt="Sardor Shop"
-                            className="w-10 h-auto"
-                        />
+  const router = useRouter(); 
 
-                        <p>Sardor shop</p>
-                    </div>
+  const handleLogout = () => {
+    router.push("/register"); 
+  };
 
-                    <div className="hidden md:flex">
-                        <NavDesktop />
-                    </div>
-                    <div className="flex items-center gap-2">
+  return (
+    <header className="w-full shadow-md sticky top-0 z-50 bg-base-300 border-b-2 border-info rounded-b-xl">
+      <Container>
+        <div className="navbar flex justify-between items-center">
+          <div className="flex items-center gap-2 cursor-pointer">
+            <img
+              src="/alifshop.svg"
+              alt="Sardor Shop"
+              className="w-10 h-auto"
+            />
+            <p>Sardor shop</p>
+          </div>
 
-                        <div>
-                            <Link href="/cart" className="text-warning "><ShoppingCart /></Link>
-                        </div>
+          <div className="hidden md:flex">
+            <NavDesktop />
+          </div>
 
-                        <div className="flex items-center p-1 flex-1 ">
-                            <ThemeController className=" flex" />
-                        </div>
+          <div className="flex items-center gap-2">
+            <Link href="/cart" className="text-warning">
+              <ShoppingCart />
+            </Link>
 
+            <div className="flex items-center p-1 flex-1">
+              <ThemeController className="flex" />
+            </div>
 
-                        <div className="dropdown dropdown-end hidden md:block">
-                            <div tabIndex={0} role="button" className="btn btn-ghost btn-circle avatar">
-                                <div className="w-10 rounded-full">
-                                    <img
-                                        alt="User avatar"
-                                        src="https://img.daisyui.com/images/stock/photo-1534528741775-53994a69daeb.webp"
-                                    />
-                                </div>
-                            </div>
-
-                            <ul
-                                tabIndex={0}
-                                className="menu menu-sm dropdown-content bg-base-content rounded-box mt-3 w-52 p-2 shadow"
-                            >
-                                <li>
-                                    <a className="justify-between text-base-300">
-                                        Profile
-                                        <span className="badge">New</span>
-                                    </a>
-                                </li>
-                                <li><a className="text-base-300">Settings</a></li>
-                                <li><a className="text-error font-bold">Logout</a></li>
-                            </ul>
-                        </div>
-                        <div className="block md:hidden">
-                            <NavMobile />
-                        </div>
-                    </div>
+            <div className="dropdown dropdown-end hidden md:block">
+              <div
+                tabIndex={0}
+                role="button"
+                className="btn btn-ghost btn-circle avatar"
+              >
+                <div className="w-10 rounded-full">
+                  <img
+                    alt="User avatar"
+                    src="https://img.daisyui.com/images/stock/photo-1534528741775-53994a69daeb.webp"
+                  />
                 </div>
-            </Container>
-        </header>
-    );
+              </div>
+
+              <ul
+                tabIndex={0}
+                className="menu menu-sm dropdown-content bg-base-content rounded-box mt-3 w-52 p-2 shadow"
+              >
+                <li>
+                  <a className="justify-between text-base-300">
+                    Profile
+                    <span className="badge">New</span>
+                  </a>
+                </li>
+                <li>
+                  <a className="text-base-300">Settings</a>
+                </li>
+                <li>
+                  <a
+                    className="text-success font-bold"
+                    onClick={handleLogout}
+                  >
+                    Register
+                  </a>
+                </li>
+              </ul>
+            </div>
+
+            <div className="block md:hidden">
+              <NavMobile />
+            </div>
+          </div>
+        </div>
+      </Container>
+    </header>
+  );
 }
